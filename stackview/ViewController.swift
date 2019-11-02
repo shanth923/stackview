48768baa60a77697c2d9ee94a75f1e290ac9d7b3
R Shantha Kumar <shanth923@achiever-104.local>
2019-11-02 13:49:00 +0530

    Initial Commit

---
+++ stackview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stackview
+//
+//  Created by R Shantha Kumar on 11/2/19.
+//  Copyright © 2019 R Shantha Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
